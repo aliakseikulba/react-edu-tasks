936e1f25b60383793bb4ee8bdaa0aa28a8efc20c
aliakseikulba <kulba.av@gmail.com>
2021-11-17 18:24:46 +0300

    fixed home page

--- src/p2-homeworks/h5/pages/HomePage/HomePage.tsx
+++ src/p2-homeworks/h5/pages/HomePage/HomePage.tsx
@@ -4,7 +4,7 @@ import s from './HomePage.module.css'
 export const HomePage = () => {
   return (
     <div className={s.wrapper}>
-      <h1>Hello This Is Home Page</h1>
+      <h1 className={s.content}>HELLO! I’m Alexey Kulba. I’m on my way to becoming a skilled frontend developer. You may see my work results in this project. Use navigation above.</h1>
     </div>
   );
 };
