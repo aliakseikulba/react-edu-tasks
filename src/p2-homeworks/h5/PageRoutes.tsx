c29bc9209e4d5d35705d8c056362d2804242adf4
aliakseikulba <kulba.av@gmail.com>
2021-11-17 12:01:16 +0300

    add pages folders

--- src/p2-homeworks/h5/PageRoutes.tsx
+++ src/p2-homeworks/h5/PageRoutes.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {Routes, Route, Link} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
+
+import {PreJunior} from './pages/PreJunior/PreJunior';
+import {Junior} from './pages/Junior/Junior';
+import {JuniorPlus} from './pages/JuniorPlus/JuniorPlus';
+import {Error404} from './pages/Error404/Error404';
 
 export const PATH = {
   PRE_JUNIOR: '/pre-junior',
@@ -10,20 +15,12 @@ export const PATH = {
 export function PageRoutes() {
   return (
     <div>
-      {/*Switch выбирает первый подходящий роут*/}
-      {/*<Switch>*/}
-
-      {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-      {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-      {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
-
-      {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
-      // add routes
-
-      {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-      {/*<Route render={() => <Error404/>}/>*/}
-
-      {/*</Switch>*/}
+      <Routes>
+        <Route path={PATH.PRE_JUNIOR} element={<PreJunior />}/>
+        <Route path={PATH.JUNIOR} element={<Junior />}/>
+        <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />}/>
+        <Route path="*" element={<Error404/>}/>
+      </Routes>
     </div>
   );
 }
